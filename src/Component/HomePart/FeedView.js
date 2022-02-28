3a6d9e3f068fbdf483e8c02421329fe0ee0fdcc9
BIKASH200 <bikashbarik16623@gmail.com>
2022-02-28 16:21:29 +0530

    Commit For leanding page

--- src/Component/HomePart/FeedView.js
+++ src/Component/HomePart/FeedView.js
@@ -1,33 +1,43 @@
 import React from "react";
-import FirstModel from "../3DModels/FirstModel";
-import tdlink from "../../3DFiles/ex1.glb" 
-
+import "../../Style/Landingpage.css";
 const FeedView = (props) => {
   return (
     <div className="mt-5 py-5">
       <div className="muscular mt-5">
-        <a href={props.link}>
-          <div class="container">
-            <div class="row">
-              <div class="col mt-5">
-                <h1 className="heading">{props.heading}</h1>
-                <p className="paragraph">{props.title}</p>
-              </div>
+        <div class="container">
+          <div class="row">
+            <div class="col mt-5">
+            <a href={props.link}>
+                <span className="textlendingp">
+                  <span className="textlen">
+                    <h1 className="heading">{props.heading}</h1>
+                    <p className="paragraph">{props.title}</p>
+                    </span>
+                  <img
+                    src="https://media1.giphy.com/media/jss17HdkspjmvaYxH1/giphy.gif?cid=6c09b952e6dc73568aa2eeb5ad68ebfd0dbccb04e7dd3c39&rid=giphy.gif&ct=s"
+                    class="midlen"
+                    height="400px"
+                    alt="..."
+                    
+                  />
+               
+                </span>
+              </a>
+            </div>
 
-              <div class="col">
-                {/* <img
-                  src={props.srclink}
-                  alt="feed image"
-                  width="550"
-                  height="400"
-                  className=" shadow-lg bg-light rounded"
-                /> */}
-                 <model-viewer id="arch_model" src={tdlink} ar ar-modes="webxr scene-viewer quick-look"  seamless-poster shadow-intensity="1" camera-controls></model-viewer>
-                {/* <FirstModel/> */}
-              </div>
+            <div class="col">
+              <model-viewer
+                id="arch_model"
+                src={props.tdink}
+                ar
+                ar-modes="webxr scene-viewer quick-look"
+                seamless-poster
+                shadow-intensity="1"
+                camera-controls
+              ></model-viewer>
             </div>
           </div>
-        </a>
+        </div>
       </div>
     </div>
   );
@@ -39,29 +49,40 @@ export const FeedViewLeft = (props) => {
   return (
     <div>
       <div className="muscular mt-5">
-        <a href={props.link}>
-          <div class="container">
-            <div class="row">
-              <div class="col">
-                {/* <img
-                  src={props.srclink}
-                  alt="feed image"
-                  width="550"
-                  height="400"
-                  className=" shadow-lg bg-light rounded"
-                /> */}
-                 <model-viewer id="arch_model" src={tdlink} ar ar-modes="webxr scene-viewer quick-look"  seamless-poster shadow-intensity="1" camera-controls></model-viewer>
-
-              </div>
-              <div class="col mt-5">
-                <h1 className="heading">{props.heading}</h1>
-                <p className="paragraph">{props.title}</p>
-              </div>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <model-viewer
+                id="arch_model"
+                src={props.tdink}
+                ar
+                ar-modes="webxr scene-viewer quick-look"
+                seamless-poster
+                shadow-intensity="1"
+                camera-controls
+              ></model-viewer>
+            </div>
+            <div class="col mt-5">
+              <a href={props.link}>
+                <span className="textlendingp">
+                  <span className="textlen">
+                    <h1 className="heading">{props.heading}</h1>
+                    <p className="paragraph">{props.title}</p>
+                    </span>
+                  <img
+                    src="https://media1.giphy.com/media/jss17HdkspjmvaYxH1/giphy.gif?cid=6c09b952e6dc73568aa2eeb5ad68ebfd0dbccb04e7dd3c39&rid=giphy.gif&ct=s"
+                    class="midlen"
+                    height="400px"
+                    alt="..."
+                    
+                  />
+               
+                </span>
+              </a>
             </div>
           </div>
-        </a>
+        </div>
       </div>
     </div>
   );
 };
-
--- src/Pages/Landingpage.js
+++ src/Pages/Landingpage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import FirstModel from "../Component/3DModels/FirstModel";
 import Feed from "../Component/HomePart/Feed";
 import HomeSlider from "../Component/HomePart/HomeSlider";
 
@@ -9,8 +8,6 @@ const Landingpage = () => {
       <div className="home_page sm: mt-5">
         <HomeSlider />
         <Feed />
-         
-      <FirstModel/>
       </div>
     </div>
   );
