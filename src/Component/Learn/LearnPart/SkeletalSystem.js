9e815facdc0b97d61a6ff2209f848bdabca318f3
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2022-03-14 20:34:13 +0530

    Added Images

--- src/Component/Learn/LearnPart/SkeletalSystem.js
+++ src/Component/Learn/LearnPart/SkeletalSystem.js
@@ -11,7 +11,7 @@ const SkeletalSystem = () => {
         <br />
         <div className="border p-5">
           <MuscularSystem
-          imgcont={"../images/ms.png"}
+          imgcont={"https://cdn.britannica.com/51/54751-050-E65F3C33/Front-views-human-skeleton.jpg"}
             heading={"Skeletal System"}
             title={
               "The skeletal system is formed of bones and cartilage, which are connected by ligaments to form a framework for the remainder of the body tissues. This article, the first in a two-part series on the structure and function of the skeletal system, reviews the anatomy and physiology of bone. Understanding the structure and purpose of the bone allows nurses to understand common pathophysiology and consider the most-appropriate steps to improve musculoskeletal health."
@@ -27,7 +27,7 @@ const SkeletalSystem = () => {
        <br/>
         <h1 className="heading mt-5 text-center">Parts of the Axial Skeleton:-</h1>
         <MuscularSystem
-        imgcont={"../images/ms.png"}
+        imgcont={"https://www.getbodysmart.com/wp-content/uploads/2017/08/Skull-bones-anterior-view-vomer-bone-divisions-745x550.png"}
           heading={"1.Vomer Bone:"}
           title={
             "The vomer is one of the unpaired facial bones of the skull. It is located in the midsagittal line, and articulates with the sphenoid, the ethmoid, the left and right palatine bones, and the left and right maxillary bones."
@@ -35,103 +35,103 @@ const SkeletalSystem = () => {
         />
 
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://i.pinimg.com/originals/c6/64/a4/c664a4bd5a9a78466ab87112b56778c0.png"} 
         heading={"2.Mandible:"} 
         title={"The mandible is the largest bone in the human skull. It holds the lower teeth in place, it assists in mastication and forms the lower jawline.  The mandible is composed of the body and the ramus and is located inferior to the maxilla. The body is a horizontally curved portion that creates the lower jawline."} 
         />
          <MuscularSystem
-         imgcont={"../images/ms.png"} 
+         imgcont={"https://images.ctfassets.net/oc83wx5cwffk/spu_wysiwyg_fid42863_asset/3e1d58c472fa9a0ede8c311e5596e3f0/cervical_spine_highlight_in_green_posterior_lateral_views_74512888_ML_Photo_Source_123RF.jpg"} 
         heading={"3.Cervical Vertebrae:"} 
         title={"The cervical spine (neck region) consists of seven bones (C1-C7 vertebrae), which are separated from one another by intervertebral discs. These discs allow the spine to move freely and act as shock absorbers during activity."} 
         />
          <MuscularSystem
-         imgcont={"../images/ms.png"} 
+         imgcont={"https://biologydictionary.net/wp-content/uploads/2020/07/ethmoidbone.jpg"} 
         heading={"4.Ethmoid Bone:"} 
         title={"The ethmoid bone is an unpaired bone in the skull that separates the nasal cavity from the brain. It is located at the roof of the nose, between the two orbits. The cubical bone is lightweight due to a spongy construction."} 
         />
          <MuscularSystem
-         imgcont={"../images/ms.png"} 
+         imgcont={"https://i.pinimg.com/originals/73/fb/21/73fb214ef7944c869a7e3a73624ed8cf.jpg"} 
         heading={"5.Sphenoid Bone:"} 
         title={"The sphenoid bone is an unpaired bone of the neurocranium. It is situated in the middle of the skull towards the front, in front of the basilar part of the occipital bone. The sphenoid bone is one of the seven bones that articulate to form the orbit."} 
         />
          <MuscularSystem
-         imgcont={"../images/ms.png"} 
+         imgcont={"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Cranial_bones_en.svg/1200px-Cranial_bones_en.svg.png"} 
         heading={"6.Parietal Bone:"} 
         title={"The parietal bones are two bones in the skull which, when joined together at a fibrous joint, form the sides and roof of the cranium. In humans, each bone is roughly quadrilateral in form, and has two surfaces, four borders, and four angles. It is named from the Latin paries, wall."} 
         />
          <MuscularSystem
-         imgcont={"../images/ms.png"} 
+         imgcont={"https://cnx.org/resources/e1cbee95a6b2d98480a003a7bc69809a859173d9/721_Rib_Cage.jpg"} 
         heading={"7.Sternum:"} 
         title={"Your sternum is a bone that's located in the middle of your chest. It's also sometimes referred to as the breastbone. Your sternum protects the organs of your torso from injury and also serves as a connection point for other bones and muscles."} 
         />
          <MuscularSystem
-         imgcont={"../images/ms.png"} 
+         imgcont={"https://cdn.britannica.com/42/133742-050-16A33263/Human-rib-cage.jpg"} 
         heading={"8.Ribs:"} 
         title={"The ribs are a set of twelve paired bones which form the protective 'cage' of the thorax. They articulate with the vertebral column posteriorly, and terminate anteriorly as cartilage (known as costal cartilage). As part of the bony thorax, the ribs protect the internal thoracic organs."} 
         />
          <MuscularSystem
-         imgcont={"../images/ms.png"} 
+         imgcont={"https://www.earthslab.com/wp-content/uploads/2018/03/Styloid-process.jpg"} 
         heading={"9.Temporal Bone:"} 
         title={"The temporal bones are situated at the sides and base of the skull, and lateral to the temporal lobes of the cerebral cortex. The temporal bones are overlaid by the sides of the head known as the temples, and house the structures of the ears. The lower seven cranial nerves and the major vessels to and from the brain traverse the temporal bone."} 
         />
            <MuscularSystem
-           imgcont={"../images/ms.png"} 
+           imgcont={"https://anatomy-medicine.com/uploads/posts/2015-08/1440064856_growth-plate-closure.jpg"} 
         heading={"10.Costal Cartilage:"} 
         title={"The costal cartilages are bars of hyaline cartilage which serve to prolong the ribs forward and contribute very materially to the elasticity of the walls of the thorax. The first seven pairs are connected with the sternum; the next three are each articulated with the lower border of the cartilage of the preceding rib; the last two have pointed extremities, which end in the wall of the abdomen."} 
         />
          <MuscularSystem
-         imgcont={"../images/ms.png"} 
+         imgcont={"https://www.researchgate.net/publication/323804926/figure/fig2/AS:631599372697629@1527596433780/Illustration-of-thoracic-vertebrae-showing-vertebral-body-pedicles-facets-transverse.png"} 
         heading={"11.Thoracic Vertebrae:"} 
         title={"Thoracic vertebrae are the twelve vertebral segments (T1-T12) that make up the thoracic spine. These structures have very little motion because they are firmly attached to the ribs and sternum."} 
         />
          <MuscularSystem
-         imgcont={"../images/ms.png"} 
+         imgcont={"https://images.ctfassets.net/oc83wx5cwffk/spu_wysiwyg_fid42884_asset/662db6e9a02f2af3d66fda4939c504e6/lumbar_spine_structures_labeled_anatomy_shutterstock_290274449.jpg?fm=webp&fit=thumb&q=65&w=884&h=645"} 
         heading={"12.Lumber Vertebrae:"} 
         title={"The lumbar vertebrae are bones that make up the spinal column or backbone, specifically within the lower back. These bones are below the cervical and thoracic vertebrae but above the sacrum or pelvis. They are the largest of the unfused vertebrae, and are larger than the bones located above."} 
         />
          <MuscularSystem
-         imgcont={"../images/ms.png"} 
+         imgcont={"https://cdn-prod.medicalnewstoday.com/content/images/articles/320/320745/diagram-of-the-chest-showing-the-xiphoid-process.jpg"} 
         heading={"13.Xiphoid Process:"} 
         title={"The xiphoid process is the most distal edge of the sternum or the breastbone. The sternum contains 3 major portions: the manubrium, the body, and the xiphoid process.The main function of the xiphoid process is to serve as a muscular attachment. It serves as an attachment on its anterior surface for the rectus abdominis muscle as well as an attachment for the aponeurosis of the internal and external oblique muscles of the abdomen."} 
         />
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://www.verywellhealth.com/thmb/PMGXa1yjHDLjgsQJHFiIcK7he8I=/2121x1193/smart/filters:no_upscale()/GettyImages-125744849-269f345b3a0c43778afcbf28a4579134.jpg"} 
         heading={"14.Sacrum:"} 
         title={"The sacrum is a shield-shaped bony structure that is located at the base of the lumbar vertebrae and that is connected to the pelvis. Joined at the very end of the sacrum are two to four tiny, partially fused vertebrae known as the coccyx or 'tail bone'."} 
         />
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://i0.wp.com/www.floridasurgeryconsultants.com/wp-content/uploads/2019/03/SI-Joint.jpg?fit=1858%2C919&ssl=1"} 
         heading={"15.Sacroiliac Joint:"} 
         title={"The sacroiliac joints link your pelvis and lower spine. They're made up of the sacrum — the bony structure above your tailbone and below your lower vertebrae — and the top part (ilium) of your pelvis. There are sacroiliac joints in both the right and left sides of your lower back."} 
         />
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://anatomy.net/uploads/8b1d12ef-8a09-4b5c-8de9-0e9367931f71.png?width=1200"} 
         heading={"16.Maxilla:"} 
         title={"The maxilla bone or maxillary bone is a fused (paired) bone that provides part or all of the bony structure of the eye sockets, the nasal passage, the hard palate, the left and right maxillary sinuses, and the upper tooth sockets. It is the second-largest facial bone. Without the maxilla, we can neither eat properly nor speak clearly. The maxilla bone is composed of a main body with four processes or projections. It borders with nine facial and cranial bones."} 
         />
          <MuscularSystem
-         imgcont={"../images/ms.png"} 
+         imgcont={"https://i.pinimg.com/originals/87/53/c3/8753c392593ca6eb028d5ff8c623c6a1.jpg"} 
         heading={"17.Sternoclavicular Joint:"} 
         title={"The sternoclavicular (SC) joint is the linkage between the clavicle (collarbone) and the sternum (breastbone). The SC joint supports the shoulder and is the only joint that connects the arm to the body."} 
         />
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://www.getbodysmart.com/wp-content/uploads/2017/07/Zygomatic-Bone-Overview-Colored-770x440.png"} 
         heading={"18.Zygomatic Bone:"} 
         title={"The zygomatic bone is a paired facial bone. Both zygoma or cheek bones are irregular and articulate with other bones of the cranium and face. They are important contributors to mastication or chewing, providing an attachment point for the masseter muscle – a jaw adductor that closes the jaw. In addition, the cheekbone contributes to the structure of the eye orbits and our ability to articulate."} 
         />
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://cdn-acgla.nitrocdn.com/bvIhcJyiWKFqlMsfAAXRLitDZjWdRlLX/assets/static/optimized/rev-5131b73/wp-content/uploads/2020/08/nasalbonecartilage.jpg"} 
         heading={"19.Nasal Bone:"} 
         title={"The nasal bone is a paired flat bone located at the upper third of the nose bridge. Each rectangular bone has an internal and external surface and four borders. These bones also feature holes (foramina) that allow veins to pass through from the skin. The thinner inferior ends of the nasal bones attach to cartilage that gives the lower two-thirds of the nose its shape."} 
         />
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://o.quizlet.com/SDEW9HHhasRH-6-508fHlA.png"} 
         heading={"20.Frontal Bone:"} 
         title={"The frontal bone (os frontale) is an unpaired craniofacial bone that provides partial coverage of the brain and forms the structure of the forehead and upper casing of the eye sockets. It is composed of a squamous part, two orbital parts, and one nasal part. Muscles attached to and surrounding the frontal bone are essential for facial expression."} 
         />
          <MuscularSystem
-         imgcont={"../images/ms.png"} 
-        heading={"21Occipital Bone:"} 
+         imgcont={"https://biologydictionary.net/wp-content/uploads/2020/06/occipitalcomputergen.jpg"} 
+        heading={"21.Occipital Bone:"} 
         title={"The occipital bone is a cranial dermal bone and the main bone of the occiput (back and lower part of the skull). It is trapezoidal in shape and curved on itself like a shallow dish. The occipital bone overlies the occipital lobes of the cerebrum. At the base of skull in the occipital bone, there is a large oval opening called the foramen magnum, which allows the passage of the spinal cord."} 
         />
         
@@ -144,42 +144,42 @@ const SkeletalSystem = () => {
        <br/>
           <h1 className="heading mt-5 text-center">Parts of the Appendicular Skeleton:-</h1>
       <MuscularSystem
-      imgcont={"../images/ms.png"} 
+      imgcont={"https://orthoinfo.aaos.org/link/f989bed708204af69ac57eb8d796594d.aspx"} 
         heading={"1.Tarsal:"} 
         title={"Tarsals are set of seven irregularly shaped bones. They are situated proximally in the foot in the ankle area.The tarsal bones of the foot are organised into three rows: proximal, intermediate, and distal."} 
         />
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://media.istockphoto.com/vectors/foot-bones-medical-illustrations-and-teaching-materials-anatomy-vector-id1315015776?k=20&m=1315015776&s=612x612&w=0&h=eox4nWc0AMdVgUu0mXFyyDRwTINU-UrxNUTKchK8CFI="} 
         heading={"2.MetaTarsal:"} 
         title={"The metatarsals are located in the forefoot, between the tarsals and phalanges. They are numbered I-V (medial to lateral).Each metatarsal has a similar structure. They are convex dorsally and consist of a head, neck, shaft, and base (distal to proximal)."} 
         />
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://totalcarepodiatry.com.au/wp-content/uploads/2020/08/ankle-anatomy.jpg"} 
         heading={"3.Phalanges:"} 
         title={"The phalanges are the bones of the fingers. The thumb has a proximal and distal phalanx, while the rest of the digits have proximal, middle and distal phalanges."} 
         />
          <MuscularSystem
-         imgcont={"../images/ms.png"} 
+         imgcont={"https://www.mountelizabeth.com.sg/images/default-source/default-album/carpal-tunnel-syndrome-what-is-it.jpg?sfvrsn=3d8d871e_4"} 
         heading={"4.Carpal:"} 
         title={"The carpal bones are bones of the wrist that connect the distal aspects of the radial and ulnar bones of the forearm to the bases of the five metacarpal bones of the hand. There are eight carpal bones, which divide into two rows: a proximal row and a distal row."} 
         />
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://www.sportsinjuryclinic.net/wp-content/uploads/2019/01/hand-bones800-800x426.jpg"} 
         heading={"5.MetaCarpal:"} 
         title={"The metacarpals are long bones within the hand that are connected to the carpals, or wrist bones, and to the phalanges, or finger bones. The metacarpals together are referred to as the ‘metacarpus.’ The tops of the metacarpals form the knuckles where they join to the wrist. On the palm side, they are covered with connective tissue."} 
         />
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Tibia_-_frontal_view.png/1200px-Tibia_-_frontal_view.png"} 
         heading={"6.Tibia:"} 
         title={"The tibia is the shinbone, the larger of the two bones in the lower leg. The top of the tibia connects to the knee joint and the bottom connects to the ankle joint. Although this bone carries the majority of the body's weight, it still needs the support of the fibula."} 
         />
          <MuscularSystem
-         imgcont={"../images/ms.png"} 
+         imgcont={"https://ars.els-cdn.com/content/image/3-s2.0-B9780123750600000073-gr19.jpg"} 
         heading={"7.Ulna:"} 
         title={"The ulna is a long bone in the forearm. It lies medially and parallel to the radius, the second of the forearm bones. The ulna acts as the stabilising bone, with the radius pivoting to produce movement."} 
         />
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://assets.ct-ortho.com/wp-content/uploads/2019/11/01192119/clavicle-resized.jpg"} 
         heading={"8.Clavical:"} 
         title={"Your collarbone (or clavicle) is the bone that runs horizontally between the top of your breastbone (sternum ) and shoulder blade (scapula). You can feel your collarbone by touching the area between your neck and your shoulder. A broken collarbone, also called a clavicle fracture, is when this bone breaks."} 
         />
--- src/Component/Learn/LearnPart/SkinAnatomy.js
+++ src/Component/Learn/LearnPart/SkinAnatomy.js
@@ -12,7 +12,7 @@ const SkinAnatomy = () => {
         <div className="border p-5">
           
           <MuscularSystem
-          imgcont={"../images/ms.png"}
+          imgcont={"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Human_skin_structure.svg/1200px-Human_skin_structure.svg.png"}
             heading={"Skin Anatomy"}
             title={
               "The muscular system is an organ system consisting of skeletal, smooth and cardiac muscles. It permits movement of the body, maintains posture and circulates blood throughout the body. ... Together with the skeletal system in the human, it forms the musculoskeletal system, which is responsible for movement of the body."
@@ -30,22 +30,22 @@ const SkinAnatomy = () => {
         <h1 className="heading mt-5 text-center">Parts of Skin:-</h1>
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://slideplayer.com/slide/14591109/90/images/17/Stratum+Germinativum+%28Basale%29.jpg"} 
         heading={"Stratum Basale:"} 
         title={" The stratum basale is a single row of columnar or cuboidal cells resting on the basement membrane zone that separates the epidermis from the dermis. Most of these cells are keratinocytes that are constantly reproducing and pushing upward to replenish the epidermal cells above."} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://i.ytimg.com/vi/M2heni5R1Sk/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLD7L1Dm7sS7OmjF8qnPEQDP6mx3dA"} 
         heading={"Keratinocyte:"} 
         title={" Keratinocytes are the main components of the epidermis. From the basal layer (stratum basale), there is a continuous formation of new cells, that scale off from the stratum corneum."} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://st3.depositphotos.com/1232814/14430/v/600/depositphotos_144302245-stock-illustration-structure-of-melanocyte.jpg"} 
         heading={"Melanocyte:"} 
         title={"Melanocytes (pigment cells) live in the stratum basale of the epidermis, between the keratinocytes. The main function of a melanocyte is the production of melanin, which is stored in the neighbouring basal keratinocytes. Dark and light skintones have the same amount of melanocytes. The production of melanin is higher in a dark skin. "} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://scx2.b-cdn.net/gfx/news/hires/2014/merkelcells.jpg"} 
         heading={"Merkel cell:"} 
         title={"Merkel-cells have a tactile function. A special type of cell found right below the epidermis (top layer of skin). These cells are very close to the nerve endings that receive the sensation of touch and may be involved in touch. "} />
         
@@ -55,62 +55,62 @@ const SkinAnatomy = () => {
         title={"Langerhans cells are antigen-presenting cells. They play an important role in the generation of an immune response against foreign antigens. "} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://www.keyence.com/Images/ss_bz-casestudy_tape-stripping_005_1919301.png"} 
         heading={"Stratum corneum:"} 
         title={" The Stratum corneum (horny layer) consists of a nuclear, completely keratinised cells, with in between them a layer consisting of keratin and lipids that let through very little water."} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://qph.fs.quoracdn.net/main-qimg-ba397fbdaf300cd1adb02d8afc166034-lq"} 
         heading={"Basement membrane of epidermis:"} 
         title={" The basement membrane is a complex structure that forms the boundary between epidermis and dermis. The function of the basement membrane is to ensure that the dermis and epidermis are attached to each other."} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://www.projectphysique.co.nz/wp-content/uploads/2019/08/AdobeStock_241414018-1-1.jpg"} 
         heading={"Fibroblast:"} 
         title={"Fibroblasts synthetise collagen, elastin, proteins and mucopolysaccharids. Together these contribute to the volume and flexibility of the dermis. "} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://static-01.hindawi.com/articles/bmri/volume-2021/8101009/figures/8101009.fig.003.svgz"} 
         heading={"Dermal matrix:"} 
         title={"The dermal matrix consists of collagen, elastin, proteins and mucopolysaccharids, that are produced by fibroblasts. "} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"http://mf3swiss.com/wp-content/uploads/2020/01/Hair-anatomy.png"} 
         heading={"Hair:"} 
         title={"Hairs are thin, keratinised skin annexes that are formed in the hair follicle. They consist of three layers: marrow, cortex and cuticle. the amount of melanin in the marrow determines the colour of the hair. Marrow can only be found in terminal hair (scalp, eye brows, armpits, genitalia) and is absent in the vellus hair. "} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://healthjade.net/wp-content/uploads/2019/06/arrector-pili-muscle.jpg"} 
         heading={"Musculus Arrector pili:"} 
         title={" Smooth muscle fibers that are attached to the hair follicle. When tightened, they cause goose bumps."} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://biologydictionary.net/wp-content/uploads/2017/06/Sebaceous-Glands.jpg"} 
         heading={"Sebaceous gland:"} 
         title={"Sebaceous glands arise from hair follicles and are often connected to the hair follicle; the gland then drains into the hair follicle. Because of the sebacious glands, the skin is covered with a thin layer of fat. The production of sebum is influenced by androgens. "} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://thumbs.dreamstime.com/b/skin-anatomy-illustration-apocrine-eccine-glands-hair-structure-layered-isolated-white-background-human-layer-follicle-168957415.jpg"} 
         heading={"Apocrine sweat gland:"} 
         title={"These glands are influenced by androgens and can be found in the external ear canal, armpit, nipple and anogenital area. The gland drains just above the drainage of the sebaceous gland. These glands contribute to the body odour. "} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://teachmeanatomy.info/wp-content/uploads/Layers-of-the-Skin-Ultrastructure.jpg"} 
         heading={"Eccrine sweat gland:"} 
         title={" These glands are present all over the body and play a role in amongst others the maintenance of elektrolyte balance and thermo regulation."} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://qph.fs.quoracdn.net/main-qimg-d5f0fb3738cb49d019fa4d680947860b-c"} 
         heading={"Nerve:"} 
         title={"Conducts signals from the skin to the brains. "} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://useruploads.socratic.org/DLGxODyHSkeUpZQbEmqp_Overhuden-epidermis.jpg"} 
         heading={"Dermal vessel:"} 
         title={"Dermal vessels supply the skin of nutrients and play an important role in thermoregulation and supply of amongst others leukocytes that are involved in the defense and inflammatory processes in the skin. "} />
         
         <MuscularSystem
-        imgcont={"../images/ms.png"} 
+        imgcont={"https://www.elephango.com/_userimages/skin3-11594.jpg"} 
         heading={"Fat in subcutis"} 
         title={"The subcutis consists of fat cells, arranged in a network of connective tissue. Blood vessels, nerves and lymphatics can be found in this network. The fat tissue functions as a resource of energy and has an isolating and protective function. "} />
         
